9bdcab3f90e944b5f21e49f8994c5b0f732cdd06
Jonathan Gamble <jdgamble555@gmail.com>
2015-05-03 10:09:23 -0700

    updated loopback

--- src/loopback.php
+++ src/loopback.php
@@ -1,11 +1,16 @@
 <?php
 
-$params = ${'_'.$_SERVER['REQUEST_METHOD']};
+$method = $_SERVER['REQUEST_METHOD'];
+
+if ($method == 'DELETE' || $method == 'PUT') die;
+
+$params = ${'_'.$method};
 
 if (empty($params)) $params = null;
 
 foreach ($params as $k => $v)
     if (!$params[$k]) $params[$k] = undefined;
 
-echo json_encode(array("Type" => $_SERVER['REQUEST_METHOD'], "parameters" => $params));
+header('Content-type: application/json');
+echo json_encode(array("Type" => $method, "parameters" => $params));
 ?>
